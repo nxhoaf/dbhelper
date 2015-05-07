c3dfbb6c0eefbf377f749f0508f6088e123c8439
nxhoaf <nxhoaf@gmail.com>
2015-05-08 00:41:28 +0700

    Draft version

--- src/main/java/com/nxhoaf/dbhelper/controller/ExtractDataControllerImpl.java
+++ src/main/java/com/nxhoaf/dbhelper/controller/ExtractDataControllerImpl.java
@@ -1,60 +1,52 @@
-package com.nxhoaf.dbhelper;
+package com.nxhoaf.dbhelper.controller;
 
 import java.io.FileOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
-
 import org.dbunit.database.DatabaseConnection;
-
 import org.dbunit.database.IDatabaseConnection;
-
 import org.dbunit.database.QueryDataSet;
-
 import org.dbunit.dataset.IDataSet;
-
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 
-
-
-
-public class ExtractTestDataSet {
+public class ExtractDataControllerImpl implements ExtractDataController {
 
     public static void main(String[] args) throws Exception {
 
         // database connection
-
         Class driverClass = Class.forName("com.mysql.jdbc.Driver");
 
         Connection jdbcConnection = DriverManager.getConnection(
-
                 "jdbc:mysql://localhost:3306/employees", "root", "");
 
         IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
 
-
-
         // partial database export
-
         QueryDataSet partialDataSet = new QueryDataSet(connection);
-
         partialDataSet.addTable("employees", "SELECT * FROM employees");
+        
+        System.out.println("All tables: ");
+        for (String table : partialDataSet.getTableNames()) {
+            System.out.println(table);
+        }
 
 //        partialDataSet.addTable("recipe_ext_xref");
-
-        FlatXmlDataSet.write(partialDataSet,
-
-                new FileOutputStream("partial-dataset.xml"));
-
-
+//        FlatXmlDataSet.write(partialDataSet, new FileOutputStream("partial-dataset.xml"));
 
         // full database export
-
         IDataSet fullDataSet = connection.createDataSet();
 
-        FlatXmlDataSet.write(fullDataSet, new FileOutputStream("full-dataset.xml"));
+//        FlatXmlDataSet.write(fullDataSet, new FileOutputStream("full-dataset.xml"));
 
+    }
+
+    public void extractPartialData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void extractAllData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
---
+++ src/main/java/com/nxhoaf/dbhelper/domain/Connection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nxhoaf.dbhelper.domain;
+
+/**
+ *
+ * @author nxhoaf
+ */
+public class Connection {
+    private String connectionString;
+    private String username;
+    private String password;
+}
